bd1e902bbf2f3185c85be93102ecd311bb4f6af7
Umar Tilmisani <umartilmisani48@gmail.com>
2026-09-11 17:47:05 +0700

    change sheet name

--- app/services/donasi_config.py
+++ app/services/donasi_config.py
@@ -17,7 +17,7 @@ KEYWORD_CATEGORIES: dict[str, list[str]] = {
         "alirkan air", "kekeringan", "sumur masjid", "sumur",
     ],
     "Gempa NTT" : ['ntt', 'gempa', 'gempa ntt', 'gem pa'],
-    "Masjid Gempa NTT": ['masjid ntt'],
+    "Masjid Gempa NTT": ['masjid ntt', 'masjid di ntt', 'masjid akib', 'ntt pembanguna'],
     "Masjid": [
         "masjid", "mesjid", "bangun", "mas jid", "wakaf", "waqaf", "donasi masjid",
         "pembangunan masjid", "pembangunan mesjid", "infaq masjid", "shodaqoh masjid",
